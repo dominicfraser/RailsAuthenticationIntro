14f38e61c856b2c0d3ba8ffae2aed874af7c4f6c
Dominic <mrdjfraser@gmail.com>
2017-06-13 16:32:57 +0100

    authentication works

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Account.destroy_all
+User.destroy_all
+
+user_one = User.create({ email: 'liz@gmail.com', password: 'password1', password_confirmation: 'password1' })
+user_two = User.create({ email: 'sam@gmail.com', password: 'password2', password_confirmation: 'password2' })
+
+user_one.accounts.create({ name: 'local', balance: 100.00 })
+user_one.accounts.create({ name: 'savings', balance: 500.00 })
+user_one.accounts.create({ name: 'offshore', balance: 400.00 })
+
+
+user_two.accounts.create({ name: 'offshore', balance: 200.00 })
